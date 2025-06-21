c41ae288ae5262d16f3e6b3a6bd920933c2acfd0
Pascal100500 <kingandbuffon@gmail.com>
2025-06-21 23:50:19 +0300

    Добавьте файлы проекта.

---
+++ View/ToDo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using ViewModel;
+
+namespace View
+{
+    /// <summary>
+    /// Логика взаимодействия для ToDo.xaml
+    /// </summary>
+    public partial class ToDo : Window
+    {
+        public ToDo()
+        {
+            InitializeComponent();
+            DataContext = new ToDoViewModel();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ ViewModel/ToDoViewModel.cs
@@ -0,0 +1,89 @@
+﻿
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ViewModel
+{
+    public class ToDoViewModel : INotifyPropertyChanged
+    {
+        private string _textTask;
+    private TaskViewModel? _selectedTask;
+
+    public ObservableCollection<TaskViewModel> Tasks { get; set; }
+
+    public TaskViewModel? SelectedTask
+    {
+        get => _selectedTask;
+        set
+        {
+            _selectedTask = value;
+            OnPropertyChanged(nameof(SelectedTask));
+        }
+    }
+
+    public string TextTask
+    {
+        get => _textTask;
+        set
+        {
+            _textTask = value;
+            OnPropertyChanged(nameof(TextTask));
+        }
+    }
+
+    public RelayCommand AddTaskCommand { get; set; }
+    public RelayCommand ToggleTaskCompletionCommand { get; set; }
+
+    public ToDoViewModel()
+    {
+        Tasks = new ObservableCollection<TaskViewModel>
+        {
+            new() 
+            { 
+                Name = "Сделать ЛЗ по WPF",
+                IsCompleted = false 
+            },
+            new()
+            { 
+                Name = "Пройти курс по С#",
+                IsCompleted = true 
+            }
+        };
+
+        AddTaskCommand = new(AddTask);
+        ToggleTaskCompletionCommand = new(ToggleTaskCompletion);
+    }
+
+        public void ToggleTaskCompletion(object? parameter)
+        {
+            if (parameter is TaskViewModel taskViewModel)
+            {
+                taskViewModel.IsCompleted = !taskViewModel.IsCompleted;
+            }
+        }
+        //public void ChangeIsComplite (TaskViewModel taskViewModel)
+        //{
+        //    taskViewModel.IsComplete = !taskViewModel.IsComplete;
+        //}
+
+        public void AddTask(object? parameter)
+        {
+            var taskViewModel = new TaskViewModel
+            {
+                Name = TextTask,
+                IsCompleted = false,
+            };
+            TextTask = string.Empty;
+            Tasks.Add(taskViewModel);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
